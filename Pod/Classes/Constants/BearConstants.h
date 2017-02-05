fa8e806097e30c83a9f9287126159eece3bbbb25
BearRan <648070256@qq.com>
2017-02-05 23:09:00 +0800

    增加物理点和像素点相应宏

--- Pod/Classes/Constants/BearConstants.h
+++ Pod/Classes/Constants/BearConstants.h
@@ -54,6 +54,7 @@ static NSString *usTest = @"usTest";
 #define SystemVersion [[[UIDevice currentDevice] systemVersion] floatValue]
 
 #define iOSVersion  [[[UIDevice currentDevice] systemVersion] floatValue]
+#define over_iOS6   ([[[UIDevice currentDevice] systemVersion] floatValue] >= 6.0)
 #define over_iOS7   ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0)
 #define over_iOS8   ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
 #define over_iOS9   ([[[UIDevice currentDevice] systemVersion] floatValue] >= 9.0)
@@ -134,6 +135,50 @@ static NSString *usTest = @"usTest";
 
 //  int 转换 NSNumber
 #define IntToNumber(int) [NSNumber numberWithInt:int]
+
+
+
+
+
+//  适配对应 高度 < 5高度时，高度 = 5高度
+#define LayOutHeight  ((HEIGHT < HEIGHT5) ? HEIGHT5 : HEIGHT)
+
+//  物理点 为单位
+#define XX_4(value)     (1.0 * (value) * WIDTH / WIDTH4)
+#define XX_5(value)     (1.0 * (value) * WIDTH / WIDTH5)
+#define XX_6(value)     (1.0 * (value) * WIDTH / WIDTH6)
+#define XX_6P(value)    (1.0 * (value) * WIDTH / WIDTH6P)
+
+#define YY_4(value)     (1.0 * (value) * LayOutHeight / HEIGHT4)
+#define YY_5(value)     (1.0 * (value) * LayOutHeight / HEIGHT5)
+#define YY_6(value)     (1.0 * (value) * LayOutHeight / HEIGHT6)
+#define YY_6P(value)    (1.0 * (value) * LayOutHeight / HEIGHT6P)
+
+
+//  像素点 为单位
+#define XX_4N(value)     (1.0 * (value) * WIDTH / NWIDTH4)
+#define XX_5N(value)     (1.0 * (value) * WIDTH / NWIDTH5)
+#define XX_6N(value)     (1.0 * (value) * WIDTH / NWIDTH6)
+#define XX_6PN(value)    (1.0 * (value) * WIDTH / NWIDTH6P)
+
+#define YY_4N(value)     (1.0 * (value) * LayOutHeight / NHEIGHT4)
+#define YY_5N(value)     (1.0 * (value) * LayOutHeight / NHEIGHT5)
+#define YY_6N(value)     (1.0 * (value) * LayOutHeight / NHEIGHT6)
+#define YY_6PN(value)    (1.0 * (value) * LayOutHeight / NHEIGHT6P)
+
+
+//  AppDelegate
+#define JKZJAPP_Delegate  (AppDelegate *)[[UIApplication sharedApplication] delegate]
+
+
+//  Font
+#define SystemFont(value)       [UIFont systemFontOfSize:value]
+#define FontSize(value)         [UIFont systemFontOfSize:(CGFloat)floor(value)]
+#define FontSize_4(value)       FontSize(XX_4(value))
+#define FontSize_5(value)       FontSize(XX_5(value))
+#define FontSize_6(value)       FontSize(XX_6(value))
+#define FontSize_6P(value)      FontSize(XX_6P(value))
+
 
 
 
