776a48326ba29675133ab3ddc6b6f525443a6341
Chobits <bearnewmac@xiongrandeMacBook-Pro.local>
2017-09-22 15:07:21 +0800

    增加BearWebView

---
+++ Pod/UIAndExtend/BearWebVC/BearWebVC.h
@@ -0,0 +1,15 @@
+//
+//  BearWebVC.h
+//  kzloan
+//
+//  Created by Chobits on 2017/9/15.
+//  Copyright © 2017年 杭州傲熠网络技术有限公司. All rights reserved.
+//
+
+#import <BearSkill/BearBaseViewController.h>
+
+@interface BearWebVC : BearBaseViewController
+
+- (instancetype)initWithURLStr:(NSString *)urlStr;
+
+@end
