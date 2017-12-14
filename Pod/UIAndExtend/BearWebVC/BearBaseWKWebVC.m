0f7a5cf8375b949bd96344815118741a92696320
Chobits <bearnewmac@xiongrandeMacBook-Pro.local>
2017-12-14 21:52:51 +0800

    调整WKWebVC

--- Pod/UIAndExtend/BearWebVC/BearBaseWKWebVC.m
+++ Pod/UIAndExtend/BearWebVC/BearBaseWKWebVC.m
@@ -41,6 +41,7 @@
 //    _urlStr = @"http://www.baidu.com";
     _urlStr = @"http://192.168.4.189:8080/#/js-bridge";
     
+    
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:_urlStr]]];
 }
 
@@ -123,37 +124,40 @@
 
 #pragma mark - WKScriptMessageHandler
 -(void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message{
-    //这里可以通过name处理多组交互
-    if ([message.name isEqualToString:@"demoName"]) {
-        //body只支持NSNumber, NSString, NSDate, NSArray,NSDictionary 和 NSNull类型
-        NSLog(@"%@",message.body);
-    }
+    
+//    //这里可以通过name处理多组交互
+//    if ([message.name isEqualToString:@"demoName"]) {
+//        //body只支持NSNumber, NSString, NSDate, NSArray,NSDictionary 和 NSNull类型
+//        NSLog(@"%@",message.body);
+//    }
     
 }
 
 #pragma mark = WKNavigationDelegate
 //在发送请求之前，决定是否跳转
 -(void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler{
-    NSString *hostname = navigationAction.request.URL.host.lowercaseString;
-    NSLog(@"%@",hostname);
-    if (navigationAction.navigationType == WKNavigationTypeLinkActivated
-        && ![hostname containsString:@".baidu.com"]) {
-        // 对于跨域，需要手动跳转
-        [[UIApplication sharedApplication] openURL:navigationAction.request.URL];
-        
-        // 不允许web内跳转
-        decisionHandler(WKNavigationActionPolicyCancel);
-    } else {
-        self.progressView.alpha = 1.0;
-        decisionHandler(WKNavigationActionPolicyAllow);
-    }
+    
+//    NSString *hostname = navigationAction.request.URL.host.lowercaseString;
+//    NSLog(@"%@",hostname);
+//    if (navigationAction.navigationType == WKNavigationTypeLinkActivated
+//        && ![hostname containsString:@".baidu.com"]) {
+//        // 对于跨域，需要手动跳转
+//        [[UIApplication sharedApplication] openURL:navigationAction.request.URL];
+//
+//        // 不允许web内跳转
+//        decisionHandler(WKNavigationActionPolicyCancel);
+//    } else {
+//        self.progressView.alpha = 1.0;
+//        decisionHandler(WKNavigationActionPolicyAllow);
+//    }
     
     
 }
 //在响应完成时，调用的方法。如果设置为不允许响应，web内容就不会传过来
 
 -(void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler{
-    decisionHandler(WKNavigationResponsePolicyAllow);
+    
+//    decisionHandler(WKNavigationResponsePolicyAllow);
 }
 //接收到服务器跳转请求之后调用
 -(void)webView:(WKWebView *)webView didReceiveServerRedirectForProvisionalNavigation:(WKNavigation *)navigation{
@@ -180,44 +184,44 @@
 
 #pragma mark WKUIDelegate
 
-//alert 警告框
--(void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler{
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"警告" message:@"调用alert提示框" preferredStyle:UIAlertControllerStyleAlert];
-    [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        completionHandler();
-    }]];
-    [self presentViewController:alert animated:YES completion:nil];
-    NSLog(@"alert message:%@",message);
-    
-}
-
-//confirm 确认框
--(void)webView:(WKWebView *)webView runJavaScriptConfirmPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(BOOL))completionHandler{
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"确认框" message:@"调用confirm提示框" preferredStyle:UIAlertControllerStyleAlert];
-    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        completionHandler(YES);
-    }]];
-    [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
-        completionHandler(NO);
-    }]];
-    [self presentViewController:alert animated:YES completion:NULL];
-    
-    NSLog(@"confirm message:%@", message);
-    
-}
-
-- (void)webView:(WKWebView *)webView runJavaScriptTextInputPanelWithPrompt:(NSString *)prompt defaultText:(nullable NSString *)defaultText initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(NSString * __nullable result))completionHandler {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"输入框" message:@"调用输入框" preferredStyle:UIAlertControllerStyleAlert];
-    [alert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
-        textField.textColor = [UIColor blackColor];
-    }];
-    
-    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        completionHandler([[alert.textFields lastObject] text]);
-    }]];
-    
-    [self presentViewController:alert animated:YES completion:NULL];
-}
+////alert 警告框
+//-(void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler{
+//    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"警告" message:@"调用alert提示框" preferredStyle:UIAlertControllerStyleAlert];
+//    [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+//        completionHandler();
+//    }]];
+//    [self presentViewController:alert animated:YES completion:nil];
+//    NSLog(@"alert message:%@",message);
+//    
+//}
+//
+////confirm 确认框
+//-(void)webView:(WKWebView *)webView runJavaScriptConfirmPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(BOOL))completionHandler{
+//    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"确认框" message:@"调用confirm提示框" preferredStyle:UIAlertControllerStyleAlert];
+//    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+//        completionHandler(YES);
+//    }]];
+//    [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+//        completionHandler(NO);
+//    }]];
+//    [self presentViewController:alert animated:YES completion:NULL];
+//    
+//    NSLog(@"confirm message:%@", message);
+//    
+//}
+//
+//- (void)webView:(WKWebView *)webView runJavaScriptTextInputPanelWithPrompt:(NSString *)prompt defaultText:(nullable NSString *)defaultText initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(NSString * __nullable result))completionHandler {
+//    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"输入框" message:@"调用输入框" preferredStyle:UIAlertControllerStyleAlert];
+//    [alert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+//        textField.textColor = [UIColor blackColor];
+//    }];
+//    
+//    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+//        completionHandler([[alert.textFields lastObject] text]);
+//    }]];
+//    
+//    [self presentViewController:alert animated:YES completion:NULL];
+//}
 
 #pragma mark - Setter & Getter
 - (WKWebViewConfiguration *)config
