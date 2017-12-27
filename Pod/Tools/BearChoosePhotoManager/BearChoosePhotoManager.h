ef4d782e3516f65319f920a6ec8ae63139c64756
Chobits <bearnewmac@xiongrandeMacBook-Pro.local>
2017-12-27 19:25:00 +0800

    初步增加ChoosePhotoManager

---
+++ Pod/Tools/BearChoosePhotoManager/BearChoosePhotoManager.h
@@ -0,0 +1,14 @@
+//
+//  BearChoosePhotoManager.h
+//  AFNetworking
+//
+//  Created by Chobits on 2017/11/15.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BearChoosePhotoManager : NSObject
+
+- (void)showActionViewInVC:(UIViewController *)inVC;
+
+@end
